3269f81e93e5810da969ba8d6a625ae341fe01e4
bharratgehlot <161546117+bharratgehlot@users.noreply.github.com>
2025-08-06 12:38:18 +0530

    Initial setup

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCFWJRw_bEYdghSURRttKlY7HQ1NyTSUkE",
+  authDomain: "student-diaries-f9758.firebaseapp.com",
+  projectId: "student-diaries-f9758",
+  storageBucket: "student-diaries-f9758.firebasestorage.app",
+  messagingSenderId: "1009801136603",
+  appId: "1:1009801136603:web:ad296b58d5272e4065bffe"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
